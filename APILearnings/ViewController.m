7e04b0e7a040c072d9536fd950a977e2a4ac7d2d
shashi kumar <shashi.kumar@horcocoasoftware.com>
2014-09-28 11:44:09 +0530

    fb button added

--- APILearnings/ViewController.m
+++ APILearnings/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UIButton *fbButton;
 @end
 
 @implementation ViewController
@@ -26,12 +26,27 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     [self setupBackgroundView];
+    [self setupFbButton];
 }
 
 - (void)setupBackgroundView {
     UIImageView *backgroundView = [[UIImageView alloc] initWithFrame:self.view.bounds];
     backgroundView.image = [UIImage imageNamed:@"blurred-background.jpg"];
     [self.view addSubview:backgroundView];
+}
+
+- (void)setupFbButton {
+    CGRect frame = CGRectMake(20, CGRectGetHeight(self.view.frame) / 2, 100, 60);
+    self.fbButton = [self buttonWithImageName:@"facebook" title:@"log in" frame:frame];
+    [self.view addSubview:self.fbButton];
+}
+
+- (UIButton *)buttonWithImageName:(NSString *)imageName title:(NSString *)title frame:(CGRect)frame {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = frame;
+    [button setImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
+    [button setTitle:title forState:UIControlStateNormal];
+    return button;
 }
 
 - (void)didReceiveMemoryWarning {
